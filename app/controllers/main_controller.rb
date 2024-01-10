1bbe9a7838a678c78d1e152b00284064a39c6283
knwokike <K.Nwokike@hvd-bb.de>
2024-01-10 14:06:14 +0100

    added validation

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
     def index
-     flash[:notice] = "Hello, Welcome"
-    flash[:alert] = "Not signed in."
+        flash[:notice] = "Hello, Welcome"
+        flash[:alert] = "Not signed in."
     end
 end
