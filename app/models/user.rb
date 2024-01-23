f99a6d923ca60bd1adbbd3dae321a31cb833c287
knwokike <K.Nwokike@hvd-bb.de>
2024-01-23 10:43:06 +0100

    Add omniauth-twitter gem and omniauth-rails_csrf_protection gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+    has_many :twitter_accounts
+    has_many :tweets
+  
     has_secure_password
-
-
-    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
-end
+  
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+  end
